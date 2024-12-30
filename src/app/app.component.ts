17e885f722b1f9db38cd84c686863858ed082d55
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-12-30 11:35:16 +0100

    server testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, Inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AsyncPipe} from '@angular/common';
+import {Firestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    AsyncPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'DABubble';
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {}
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
+import {Injectable} from '@angular/core';
+import {collection, Firestore} from '@angular/fire/firestore';
+import {addDoc} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  firestore = inject(Firestore);
+
+
+  async addTestEntry(): Promise<void> {
+    const testEntry = {
+      name: 'Test',
+      timestamp: new Date(),
+    };
+
+    try {
+      let collectionRef = collection(this.firestore, 'testCollection');
+      await addDoc(collectionRef, testEntry);
+
+      console.log('Testeintrag erfolgreich erstellt!');
+    } catch (error) {
+      console.error('Fehler beim Erstellen des Testeintrags:', error);
+    }
+  }
 }
